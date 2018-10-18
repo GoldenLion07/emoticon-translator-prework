41089625645df99b540c8802c23b8fa64a4a1760
Scott M <gmat191@gmail.com>
2018-10-18 04:38:32 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,3 +37,7 @@ def get_english_meaning
   result
 
 end
+
+test_emoticons = {"angel"=>["O:)", "japanese_angel"], "angry"=>[">:(", "japanese_angry"], "bored"=>[":O", "japanese_bored"], "confused"=>["%)", "japanese_confused"]}
+
+load_library(test_emoticons)
