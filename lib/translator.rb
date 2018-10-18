728f74e425c68a7a541afbf5dffd8206eea0e59e
Scott M <gmat191@gmail.com>
2018-10-18 04:36:20 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,4 +30,11 @@ end
 
 def get_english_meaning
   # code goes here
+    emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
+
 end
