d68da952eb4d8357499a5a6a0d941a92d630df9b
Scott M <gmat191@gmail.com>
2018-10-18 04:45:27 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def load_library(path)
   emoticon_hash
 end
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon(path, emoticon)
   # code goes here
  library = load_library(file_path)
   library["get_emoticon"].each do |key, value|
